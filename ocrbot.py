f1c9460e03a7eebb15b5a5d40721917b245762cb
cyberboysumanjay <cyberboysumanjay@gmail.com>
2020-12-06 23:05:33 +0530

    Updated Code

--- ocrbot.py
+++ ocrbot.py
@@ -1,64 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=W0613, C0116
+# type: ignore[union-attr]
+
+import logging
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
-from telegram.ext.dispatcher import run_async
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-from telegram import Update, Bot, ParseMode
+import constants
 import os
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi! \n\nWelcome to Optical Character Recognizer Bot. \n\nJust send a clear image to the bot and it will recognize the text in the image and send it as a message!\nTo get my contact details tap /contact \nTo get donation details tap /donate\n')
+    update.message.reply_text(constants.welcome_text)
 
-def donate(bot, update):
-    """Send a message when the command /donate is issued."""
-    bot.send_message(chat_id=201566591,text=str(update.message.chat_id)+" "+str(update.message.from_user.name)+' clicked /donate ')
-    update.message.reply_text("To donate you can send any amount you wish to me using the following *Payment Options*:\n\nUPI - `sumanjay@ybl` (Long press to copy)\n[Paytm](https://p-y.tm/SnE-jE9)\n[PayPal](https://paypal.me/sumanjay/10USD)\n[Debit/Credit/Netbanking/Other Wallets](https://www.instamojo.com/@sumanjay)\n\nAfter making payment don't forget to send a screenshot of the Transaction to @cyberboysumanjay", parse_mode=ParseMode.MARKDOWN)
 
-def contact(bot, update):
-    """Send a message when the command /contact is issued."""
-    update.message.reply_text("Heya! You can find me on \n[Telegram](https://telegram.me/cyberboysumanjay)\n[Facebook](https://facebook.com/imsumanjay)\n[Instagram](https://instagram.com/sumanjay_)\n[Twitter](https://twitter.com/cyberboysj)\n[LinkedIn](https://www.linkedin.com/in/sumanjay-9816a4128)", parse_mode=ParseMode.MARKDOWN)
+def donate(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(constants.donate_text, parse_mode=ParseMode.MARKDOWN)
 
-def search(bot, update):
+
+def read_image(update: Update, context: CallbackContext) -> None:
     """Send reply of user's message."""
-    photo_file = bot.get_file(update.message.photo[-1].file_id)
-    photo_file.download('testing.jpg')
+    chat_id = update.message.chat_id
     try:
-        output=pytesseract.image_to_string(Image.open('testing.jpg'))
-        update.message.reply_text('`'+str(output)+'`',parse_mode=ParseMode.MARKDOWN,reply_to_message_id=update.message.message_id)
+        photo_file = update.message.photo[-1].get_file()
+        img_name = str(chat_id)+'.jpg'
+        photo_file.download(img_name)
+        output=pytesseract.image_to_string(Image.open(img_name))
+        if output:
+            update.message.reply_text('`'+str(output)+'`\n\nImage to Text Generated using @imagereaderbot', parse_mode=ParseMode.MARKDOWN, reply_to_message_id = update.message.message_id)
+        else:
+            update.message.reply_text(constants.no_text_found)
     except Exception as e:
-        update.message.reply_text(e)
+        update.message.reply_text("Error Occured: `"+str(e)+"`")
+    finally:
         try:
-            os.remove('testing.jpg')
+            os.remove(img_name)
         except Exception:
             pass
 
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+def reply_to_text_message(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(constants.reply_to_text_message)
+
 
 def main():
     """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
     ocr_bot_token=os.environ.get("BOT_TOKEN", "")
-    updater = Updater(ocr_bot_token)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("donate", donate))
-    dp.add_handler(CommandHandler("contact", contact))
-    dp.add_handler(MessageHandler(Filters.photo, search))
-    dp.add_error_handler(error)
+    updater = Updater(ocr_bot_token, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("donate", donate))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_text_message))
+    dispatcher.add_handler(MessageHandler(Filters.photo, read_image))
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
